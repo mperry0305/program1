62f91cc638277de4d7610adafc88b58c6361fe86
mperry0305 <mperry0305@gmail.com>
2013-02-14 12:02:09 -0500

    Update letterPattern.java

--- letterPattern.java
+++ letterPattern.java
@@ -114,7 +114,7 @@ public void extractFeatures(){
              c10=grid[r][c+9];
              c11=grid[r][c+10];
 
-             massbottom = massbottom+c1+c2+c3;
+             massbottom = massbottom+c1+c2+c3+c4+c5+c6+c7+c8+c9+c10+c11;
              isMB=true;
          }
          if(grid[r][c]==1){
